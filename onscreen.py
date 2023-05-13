20f314b448bbff94b0ff10df39ed5182975bf1dc
Kodex <gcavedal@gmail.com>
2023-05-12 23:45:43 -0300

    PIL.ImageFont led_8x6.ttf

--- onscreen.py
+++ onscreen.py
@@ -1,44 +1,43 @@
 import os
 import cv2
+import json
 import PIL.ImageFont
 from loguru import logger
 
 
 def get_price_rate():
-    # should be imported from prices.json
-    data = {
-        "dolar oficial": "$238.00",
-        "euro oficial": "$329.19",
-        "pesos chilenos": "$3.09",
-        "dolar informal": "$411.00",
-    }
+    data = json.load(open(os.path.join("src", "prices.json")))
     return data
 
 
-# draw the exchange rate board using cv2 and PIL TrueType fonts
 def draw_price_board(frame):
-    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_path = os.path.join("src", "led_8x6.ttf")
+    font_size = 32
+    font = PIL.ImageFont.truetype(font_path, font_size)
     font_scale = 1
     color = (250, 250, 250)
     spacing_v = 50
-    spacing = 300
+    column_spacing = 300
     margin_left = 50
     margin_top = 20
     tickness = 2
+
+    loc_currency = (margin_left, margin_top + (i + 1) * spacing_v)
+    loc_price = (margin_left + column_spacing, margin_top + (i + 1) * spacing_v)
 
     data = get_price_rate()
 
     for i in range(4):
         currency = list(data.keys())[i]
         price = list(data.values())[i]
-        cv2.putText(frame, currency, (margin_left, margin_top + (i + 1) * spacing_v), font, font_scale, color, tickness)
-        cv2.putText(
-            frame, price, (margin_left + spacing, margin_top + (i + 1) * spacing_v), font, font_scale, color, tickness
-        )
+        # Currency
+        cv2.putText(frame, currency, loc_currency, font, font_scale, color, tickness)
+        # Value in ARS
+        cv2.putText(frame, price, loc_price, font, font_scale, color, tickness)
 
 
-# iterator for background video (return: cap)
 def get_video_capture_object():
+    """iterator for background video (return: cap)"""
     src_dir = "src"
     video_files = os.listdir(src_dir)
     for video_file in video_files:
@@ -59,17 +58,16 @@ def set_video_to_full_screen(video_capture_object):
 
 def play_video_loop():
     while True:
-        for video_capture_object in get_video_capture_object():  # cycle through all the backgrounds videos
-            while True:  # cycle through frames
-                ret, frame = video_capture_object.read()
+        for cap in get_video_capture_object():  # cycle through videos in src folder
+            set_video_to_full_screen(cap)
+            while True:  # cycle through each video frame
+                ret, frame = cap.read()
                 if not ret:
                     break
-
                 draw_price_board(frame)
                 cv2.imshow("Video", frame)
-
                 key = cv2.waitKey(30)
-                if key == 27:
+                if key == 27:  # Esc
                     exit()
 
 
