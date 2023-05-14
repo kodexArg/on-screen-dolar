b0d6abc9814636d58b118ff7df7a3a27d1d530ed
Kodex <gcavedal@gmail.com>
2023-05-14 03:25:08 -0300

    roadmap-3

--- bot.py
+++ bot.py
@@ -11,31 +11,24 @@ from telegram.ext import (
     filters,
 )
 
-# Strings this bot uses, so that it can be easily changed for other languages.
+logger.add("logs/bot.log", rotation="500 MB")
+
+# Strings used. Change as you wish.
 dialog = {
     "change": "Cambiar",  #  This is the text of the command that preceed the currency to change (startswitch)
     "hello": "Hola!",
-    "received": "Mensaje recibido de ",
+    "received": "Mensaje recibido de",
     "bye": "Adios!",
     "changing_prices": "Cambiando tablero de precios",
     "price_updated": "Valor actualizado",
-    "not_found": "No se encontró el valor",
-    "format_error": "Formato de mensaje incorrecto o la moneda no existe. Se espera: 'Cambiar <moneda> <precio>'",
-    "error_nothing_changed": "Error inesperado. No se realizón ningán cambio",
+    "currency_not_found": "La moneda no se encuentra en la lista",
+    "format_err": "Formato de mensaje incorrecto o la moneda no existe. Se espera: 'Cambiar <moneda> <precio>'",
+    "unknown_err": "Error inesperado. No se realizó ningún cambio",
 }
 
 
 class ExchangePriceBoard:
-    """
-
-    This is an example of the json/dictonary used:
-    {
-        "DOLAR": "382 ARS",
-        "EURO": "293 ARS",
-        "PESOS CHILENOS": "3.16 ARS",
-        "DOLAR BLUE": "411 ARS"
-    }
-    """
+    # TODO: docstring here
 
     def __init__(self):
         self.prices = {}
@@ -51,19 +44,29 @@ class ExchangePriceBoard:
         return " ".join(f"{key}:{value}" for key, value in self.prices.items())
 
     def set_price(self, currency: str, price: str) -> Tuple[bool, str]:
-        """
-        This method is used to change the price of a currency.
-        It returns a tuple with the status of the operation and a message to send to the user.
-        """
-        # check if currency is in the dictionary
-        if currency.capitalize() not in self.prices.keys():
-            return False, f"{currency} {dialog['not_found']}"
+        """Returns a tuple with the result of the operation and a message to send"""
+        currency = currency.upper()
+
+        # check if currency is in the list
+        if currency not in self.prices.keys():
+            return False, f"{currency} {dialog['currency_not_found']}"
+
+        # check if price can be converted to float
+        try:
+            _ = float(price)
+        except ValueError:
+            return False, f"{price} {dialog['format_err']}"
 
         # update price
         self.prices[currency] = f"{price} ARS"
 
+        # overwrite json file
+        with open(os.path.join("src", "prices.json"), "w") as file:
+            file.write(json.dumps(self.prices))
+            logger.info("Prices updated")
+
         # return the new price
-        return True, f"{currency} {dialog['price_updated']}: {price} ARS"
+        return True, f"<{currency}> {dialog['price_updated']}: {price} ARS"
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -87,22 +90,20 @@ async def listener(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # TODO: add docstring here
 
     user = update.message.from_user
+    split_msg = update.message.text.split(" ")
 
     async def incorrect_format():
-        """Send a message and log when the format of the message is incorrect."""
-        logger.info(
+        """Common function to handle format issues in the text message"""
+        logger.warning(
             f"{dialog['received']} {user.first_name} {user.last_name} ({user.id}): {update.message}"
         )
-        await update.message.reply_text(
-            f"{dialog['format_error']} \n {update.message.text.split(' ')}"
-        )
+        await update.message.reply_text(f"{dialog['format_err']} \n {split_msg}")
 
-    # Is authorized
-    if not is_authorized_user(user.id) or message is None:
+    # Authorization check
+    if not is_authorized_user(user.id):
         return
 
-    # Exit if message does not start with "Cambiar" or don't have 3 words
-    split_msg = update.message.text.split(" ")
+    # Format check (starts with "Cambiar" and has 3 words)
     if not update.message.text.startswith(dialog["change"]) or len(split_msg) != 3:
         await incorrect_format()
         return
@@ -120,11 +121,12 @@ async def listener(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # change price
     set_price_success, message = epb.set_price(currency, price)
     if not set_price_success:
-        await update.message.reply_text(dialog["error_nothing_changed"])
+        logger.error(f"Error: {message}")
+        await update.message.reply_text(f"Error\n{message}")
         return
-    
+
     # new price
-    await update.message.reply_text(f"{dialog['price_updated']} \n {epb.get_prices()}")
+    await update.message.reply_text(f"{message} \n\n {epb.get_prices()}")
 
 
 def main() -> None:
